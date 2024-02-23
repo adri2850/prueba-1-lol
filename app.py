986c1de7844ee7ec08dacf29809a9c13ccc6c183
adri2850 <161059086+adri2850@users.noreply.github.com>
2024-02-23 10:42:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
+st.write("Estadística de goles de Messi")
+
+# Cargar los datos desde la página web
+pagina = 'https://players.fcbarcelona.com/es/jugador/548-messi-lionel-andres-messi-cuccitini'
+tablas = pd.read_html(pagina)
+
+# Crear la aplicación web con Streamlit
+st.write(tablas[0].head(5))
+
+# Mostrar estadística gráfica
+st.write("Estadística gráfica")
+
+# Obtener los datos para el gráfico
+y = tablas[0]['674Goles']
+x = tablas[0]['18TEMPORADAS']
+
+# Crear el histograma
+fig, ax = plt.subplots()
+ax.hist(y, bins=50)
+
+# Mostrar el gráfico en la aplicación web
+st.pyplot(fig)
