f68d0df2a49a98a222a7f9fc76ee1bc8cb0d96ff
adri2850 <161059086+adri2850@users.noreply.github.com>
2024-02-23 10:58:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,16 +11,3 @@ tablas = pd.read_html(pagina)
 # Crear la aplicación web con Streamlit
 st.write(tablas[0].head(5))
 
-# Mostrar estadística gráfica
-st.write("Estadística gráfica")
-
-# Obtener los datos para el gráfico
-y = tablas[0]['674Goles']
-x = tablas[0]['18TEMPORADAS']
-
-# Crear el histograma
-fig, ax = plt.subplots()
-ax.hist(y, bins=50)
-
-# Mostrar el gráfico en la aplicación web
-st.pyplot(fig)
